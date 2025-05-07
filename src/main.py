6db2b8b90791f175f3f948be36ba9d5d4f57acda
Daniel-J-Manning <emaildanieljmanning@gmail.com>
2025-05-07 19:52:44 +0100

    chore(main): add main file; import needed libs

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# ============================ IMPORTS ============================ #
+from string import (
+    ascii_lowercase,                # A-Z
+    ascii_uppercase,                # a-z
+    digits,                         # 0-9
+    punctuation)
+# Import module to select random choice
+from secrets import choice
+# ============================ IMPORTS ============================ #
+
