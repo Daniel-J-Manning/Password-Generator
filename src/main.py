20f465f2e0cc0493a1775d1e8395d433cea04627
Daniel-J-Manning <emaildanieljmanning@gmail.com>
2025-05-07 20:23:14 +0100

    chore(main): add spaces, comments and gen_pwd func

--- src/main.py
+++ src/main.py
@@ -8,5 +8,28 @@ from string import (
 from secrets import choice
 # ============================ IMPORTS ============================ #
 
+
+# =========================== CONSTANTS =========================== #
 CHARS = (ascii_lowercase + ascii_uppercase + digits + punctuation)
-password = ""
+# =========================== CONSTANTS =========================== #
+
+
+# ======================= Generate Function ======================= #
+def generate_pwd() -> str:
+    """
+    Prompts the user for password length and returns a generated password string.
+    """
+    try:
+
+        length = int(input("How long would you like your password to be (Minimum 8): "))
+
+        if length < 8:
+            raise ValueError("Password length must be at least 8 characters.")
+        
+        password = ''.join(choice(CHARS) for _ in range(length))
+        return password
+    
+    except ValueError as e:
+        print("Error:", e)
+        return ""
+# ======================= Generate Function ======================= #
