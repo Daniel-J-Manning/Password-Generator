d9909beadae20f0ca05eb6d0c6da5f46a80c6338
Daniel-J-Manning <emaildanieljmanning@gmail.com>
2025-05-13 17:14:52 +0100

    chore(generate): add cython types for gen_pwd func

--- src/Functions/generate.py
+++ src/Functions/generate.py
@@ -1,3 +1,4 @@
+import cython as C
 from secrets import choice
 from string import (
     ascii_letters,
@@ -5,9 +6,9 @@ from string import (
     punctuation
 )
 
-CHARS = (ascii_letters + digits + punctuation)
+CHARS: C.char[100] = (ascii_letters + digits + punctuation)
 
 # ======================= Generate Function ======================= #
-def generate_pwd(length: int) -> str:
+def generate_pwd(length: C.int) -> C.char[100]:
     return ''.join(choice(CHARS) for _ in range(length))
 # ======================= Generate Function ======================= #
